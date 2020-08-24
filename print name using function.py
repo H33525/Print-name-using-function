7c1754d144163bab40e8d14319de0590e3734b93
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-08-24 10:00:18 +0530

    Add files via upload

---
+++ print name using function.py
@@ -0,0 +1,14 @@
+class playercharacter:
+
+def__init__(self,name):
+self.name= name   #attributes
+
+def rum(self):
+  print('rum')
+player1= playercharacter('Cindy')
+print(player1.name)
+
+player2= playercharacter('Tom')
+print(player2.name)
+
+
